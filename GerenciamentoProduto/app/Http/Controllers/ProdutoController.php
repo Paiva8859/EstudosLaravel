29a13b287c18b9c434cd9f0be8a7c53ce15d6af8
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-08-05 16:34:53 -0300

    a

--- GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
+++ GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
@@ -7,12 +7,59 @@ use App\Models\Produto;
 
 class ProdutoController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $produtos = Produto::all();
         return view('produtos', compact('produtos'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('produtos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal',
+        ]);
+
+        Produto::create($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto criado com sucesso.');
+    }
+
+    public function show(Produto $produto)
+    {
+        return view('produtos.show', compact('produto'));
+    }
+
+    public function edit(Produto $produto)
+    {
+        return view('produto.edit', compact('produto'));
+    }
+
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preço' => 'required|numeric',
+            'descricao' => 'required|integer'
+        ]);
+
+        $produto->update($request->all());
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto atualizado com sucesso.');
+    }
+
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto Deletado com Sucesso.');
     }
 }
